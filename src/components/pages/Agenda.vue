a8d00114e000dfee8cddc4599ceed6b6a368016d
giliolizera <guilhermegilioli93@gmail.com>
2023-10-31 14:39:17 -0300

    WIP

--- src/components/pages/Agenda.vue
+++ src/components/pages/Agenda.vue
@@ -6,7 +6,13 @@
       <UpSideMenu />
     </div>
     <div class="text-slate-800 bg-white rounded dark:bg-slate-800 dark:text-white">
-      <Calendar/> 
+      <div class="divide-y max-w-md bg-white dark:bg-slate-800 p-5">
+        <p class="font-medium text-xl pb-1">Sistema de Agendamentos</p>
+        <p class="text-base font-thin py-2">Confira aqui, a data das suas próximas limpezas, local, produtos que deverá levar...</p>
+      </div>
+      <div class="flex justify-start">
+        <Calendar/> 
+      </div>
     </div>
   </template>
   
