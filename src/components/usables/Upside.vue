b98414c73c1e47a2e3ec0902022397d91fed9736
giliolizera <guilhermegilioli93@gmail.com>
2023-11-15 14:05:52 -0300

    feat: update Upside Menu

--- src/components/usables/Upside.vue
+++ src/components/usables/Upside.vue
@@ -35,9 +35,9 @@
                   Produtos
                </button>
             </router-link>
-            <router-link to="/compra-produto" class="mt-3">
+            <router-link to="/limpeza" class="mt-3">
                <button class="dark:bg-slate-900 bg:gray-200 dark:text-gray-100 font-medium py-2 px-5 mb-2 rounded-xl">
-                  Compra Produtos
+                  Limpeza
                </button>
             </router-link>
             <div class="ml-2 mt-6">
