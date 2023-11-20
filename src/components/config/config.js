8e59039b6951bdf9916ca179f5c22857ec5ec429
giliolizera <guilhermegilioli93@gmail.com>
2023-11-20 11:06:27 -0300

    vercel db.json

--- src/components/config/config.js
+++ src/components/config/config.js
@@ -1,4 +1,4 @@
 export default {
-    API_URL: 'http://localhost:3000'
-    // API_URL: 'https://json-server-kohl-tau.vercel.app/api'
+    // API_URL: 'http://localhost:3000'
+    API_URL: 'https://json-server-kohl-tau.vercel.app/api'
 }
