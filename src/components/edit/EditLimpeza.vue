3102b2da32b1d461639609959f08c64e75ffc60a
Guilherme Gilioli <116670240+giliolizera@users.noreply.github.com>
2023-11-28 10:12:51 -0300

    Update EditLimpeza.vue

--- src/components/edit/EditLimpeza.vue
+++ src/components/edit/EditLimpeza.vue
@@ -115,11 +115,7 @@
                   class="w-full dark:text-gray-200 dark:border-blue-600 border-black border dark:bg-slate-700 bg-white rounded-md p-2 pl-3 mt-1"
                   v-model="form.horario">
             </div>
-            <div class="lg:col-span-2">
-               <div class="text-sm font-medium flex space-x-1 pl-1 mt-2">
-                  <p>Status</p><p class="flex text-red-600">*</p>
-               </div>
-               <div class="flex justify-between space-x-2 mt-4">
+           <div class="flex justify-between space-x-2 mt-4">
                   <button 
                   @click.prevent="form.status = 'Cancelado'"
                   :class="form.status === 'Cancelado' ? 'p-2 rounded-xl border border-rose-900 font-medium w-36 md:w-40 lg:w-60 2xl:w-72 h-12 bg-red-600' : 'p-2 rounded-xl border border-rose-900 font-medium w-36 md:w-40 lg:w-60 2xl:w-72 h-12 bg-red-600/30'"
